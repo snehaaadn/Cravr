b1fd5409f86c8b98a45a763c10ac14e8aecc4d6a
Tushar Gupta <tushargupta@Tushars-MacBook-Air-2.local>
2025-12-11 12:38:19 +0530

    update header

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Homepage from './pages/homepage'
+import Header from './components/common/header'
+import Footer from './components/common/footer'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <Header />
       <Homepage />
+    <Footer />
     </>
   )
 }
