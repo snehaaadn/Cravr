6b768ca49ca84216ab5ec407223278187459d331
Tushar Gupta <tushargupta@Tushars-MacBook-Air-2.local>
2025-12-12 15:27:20 +0530

    Add Profile Page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,21 +4,25 @@ import Homepage from './pages/homepage'
 import Aboutpage from './pages/aboutpage'
 import Header from './components/common/header'
 import Footer from './components/common/footer'
+import Profilepage from './pages/profilepage'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/about" element={<Aboutpage />} />
-      </Routes>      
-    <Footer />
-    </>
-  )
+      <div className='flex flex-col min-h-screen '>
+        <Header />
+        <main className="flex-1 container mx-auto px-2 py-6">
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/about' element={<Aboutpage />} />
+            <Route path='/profile' element={<Profilepage />} />
+            <Route path='*' element={<Navigate to='/' />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+  );
 }
 
 export default App
--- Frontend/src/pages/homepage.jsx
+++ Frontend/src/pages/homepage.jsx
@@ -2,6 +2,10 @@ import react from "react";
 function Homepage(){
     return(
         <>
+        <div>
+            <h1 className="font-bold text-4xl">Welcome to Cravr!</h1>
+            <p className="mt-4 text-lg">Discover the best restaurants and dishes around you.</p>
+        </div>
         </>
     )
 }
